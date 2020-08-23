b3b14d16e379dfb79d6b90d5cb0eef98a2885734
Ahmed Bankole <thegeekyahmed@gmail.com>
2020-08-23 01:11:38 +0100

    import bootstrap and font-awesome dependencies

--- src/App.vue
+++ src/App.vue
@@ -18,13 +18,12 @@ export default {
 };
 </script>
 
-<style>
+<style lang="less">
+@import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+@import "../node_modules/@fortawesome/fontawesome
+free/css/all.min.css";
 #app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  background: #eeeeee;
+  min-height: 100vh;
 }
 </style>
