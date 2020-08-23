2dd44690e52873f0fca0dd2e8a5d08e04410a1dc
Ahmed Bankole <thegeekyahmed@gmail.com>
2020-08-23 01:07:23 +0100

    create Header.vue and GitHubExplorer.vue

---
+++ src/components/GithubExplorer.vue
@@ -0,0 +1,10 @@
+<template>
+  <div></div>
+</template>
+<script>
+export default {
+  name: "GithubExplorer"
+};
+</script>
+<!-- Add "scoped" attribute to limit CSS to this component only --><style scoped lang="less">
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,10 @@
+<template>
+<div></div>
+</template>
+<script>
+export default {
+name: "Header" 
+};
+</script>
+<!-- Add "scoped" attribute to limit CSS to this component only --><style scoped lang="less">
+</style>
